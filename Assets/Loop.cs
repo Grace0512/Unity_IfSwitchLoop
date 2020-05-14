1649b291b6f637d812df43e253ea07811257a37b
grace <lov011052@gmail.com>
2020-05-14 09:19:12 +0800

    C#作業

--- Assets/Loop.cs
+++ Assets/Loop.cs
@@ -10,18 +10,14 @@ public class Loop : MonoBehaviour
     }
 
     
-    void Update()
-    {
-        
-    }
-
+   
     void SetTriangle()
     {
         for(int i = 0; i <= 10; i++)
         {
             for(int j=0;j<i;j++)
             {
-                Instantiate(cube, new Vector3(i * 2+1000, j*2+500, -200), Quaternion.Euler(0, 0, 0));
+                Instantiate(cube, new Vector3(i * 2, j*2,1 ), Quaternion.Euler(0, 0, 0));
             }
             
         }
